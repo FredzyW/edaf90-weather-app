33cba1e808fa33bd6a76f75d8da4fc89580cb6da
= <emil.ronkainen@gmail.com>
2023-03-01 09:18:58 +0100

    add api calls in forecast component

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -1,4 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { ApiService } from '../api.service';
+
+type Weather = {
+  temp: number,
+  feels_like: number,
+  temp_max: number,
+  temp_min: number,
+  weather: {
+    description: string,
+    icon: string,
+    id: number,
+    main: string
+  },
+  wind_speed: number,
+  date: string
+}
 
 @Component({
   selector: 'app-forecast',
@@ -7,6 +23,9 @@ import { Component, Input } from '@angular/core';
 })
 export class ForecastComponent {
   private _location: string;
+  forecast: Weather[]
+
+  constructor(private apiService: ApiService) { }
 
   get location() {
     return this._location;
@@ -14,5 +33,16 @@ export class ForecastComponent {
 
   @Input() set location(val: string) {
     this._location = val;
+
+    this.apiService
+      .getForecastByCity(this._location)
+      .then(data => {
+        this.forecast = data as Weather[];
+      })
+      .catch(e => console.error(e));
+  }
+
+  getImageLink(icon: string) {
+    return `http://openweathermap.org/img/wn/${icon}@2x.png`;
   }
 }
